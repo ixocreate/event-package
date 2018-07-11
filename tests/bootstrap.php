749559c352218f02117212d7318586abe1f1c843
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-07-11 14:37:40 +0200

    initial import

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * kiwi-suite/event (https://github.com/kiwi-suite/event)
+ *
+ * @package kiwi-suite/event
+ * @link https://github.com/kiwi-suite/event
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+\chdir(\dirname(__DIR__));
+include 'vendor/autoload.php';
